a65f373cd5d7d009c3b163e5ea8fae74b1c22a05
Monique.Bass01 <monique.bass01@gmail.com>
2020-08-03 16:59:55 -0500

    Add project files.

---
+++ Validator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a pin code between 4 and 8 digits.");
+            string pincode = Console.ReadLine();
+            int minLength = 8;
+            int pincodeLength = pincode.Length;
+            bool valid = true;
+
+            foreach (var c in pincode)
+            {
+                if (!char.IsDigit(c))
+                {
+                    valid = false;
+                    break;
+                }
+            }
+            if (pincodeLength < 4 || pincodeLength>= minLength || !valid)
+            {
+                valid = false;
+                Console.WriteLine("The pincode should be between 4 and 8 digits and not characters!");
+            }
+            else
+
+            {
+
+              Console.WriteLine(valid);
+            }
+        }
+    }
+}
